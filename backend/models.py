883b32ac02834c613860bddae10f2b2ff9d59dbf
lowsucae <lowie.sucaet@ugent.be>
2025-04-14 22:57:32 +0200

    changed max length password_hash

--- backend/models.py
+++ backend/models.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
